29de60996bc4c6ccd399fc3ebabd072e4528f8b2
mohmyyy <iammohmy@gmail.com>
2023-03-16 18:03:55 +0530

    Shoppers Stop for Tshirts

--- src/components/store/CartContextProvider.js
+++ src/components/store/CartContextProvider.js
@@ -1,17 +1,50 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import cartContext from "./cart-context";
 
 const CartContextProvider = (props) => {
   const [item, setItem] = useState([]);
   const [cart, setCart] = useState([]);
 
-  const addItemHandler = (item) => {
-    setItem((PrevValue) => {
-      return [...PrevValue,item]
-    })
+  useEffect(() => {
+    const asyncFun = async () => {
+      let response = await fetch(
+        `https://crudcrud.com/api/0efa045cc3044261beb159d1085fbb0f/cart`
+      );
+      let data = await response.json();
+      setItem([...data]);
+    };
+    asyncFun();
+  }, []);
+  
+  useEffect(() => {
+    const asyncFun = async () => {
+      let response = await fetch(
+        `https://crudcrud.com/api/0efa045cc3044261beb159d1085fbb0f/Carts`
+      );
+      let data = await response.json();
+      setCart([...data]);
+    };
+    asyncFun();
+  }, []);
+
+  const addItemHandler = async (item) => {
+    let response = await fetch(
+      `https://crudcrud.com/api/0efa045cc3044261beb159d1085fbb0f/cart`,
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setItem((prevValue) => {
+      return [...prevValue, data];
+    });
   };
 
-  const addToCartHandler = (item)=>{
+  const addToCartHandler = async (item) => {
     // console.log(item)
     // console.log(cartObj.items)
     // const updatedItems = cartObj.items.map((itm) => {
@@ -22,20 +55,30 @@ const CartContextProvider = (props) => {
     //   }
     // })
     // console.log(updatedItems)
-    setCart((PrevValue)=>{
-      return [...PrevValue, item]
-    })
-  }
+    let response = await fetch(
+      `https://crudcrud.com/api/0efa045cc3044261beb159d1085fbb0f/Carts`,
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setCart((prevValue) => {
+      return [...prevValue, data];
+    });
+  };
   const cartObj = {
     items: item,
     addItems: addItemHandler,
-    cart:cart,
-    addToCart:addToCartHandler,
+    cart: cart,
+    addToCart: addToCartHandler,
   };
 
-  console.log(cartObj.items)
-  console.log(cartObj.cart)
-  
+  console.log(cartObj.items);
+  console.log(cartObj.cart);
 
   return (
     <cartContext.Provider value={cartObj}>
